52af74194db1f47950727be826593f414714945e
Ryoma <ryoma@40-60studio.com>
2025-07-02 12:01:42 +0200

    Add dist folder

---
+++ dist/a-propos/notre-equipe.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports={}:"function"==typeof define&&define.amd?define("a-propos/notre-equipe",[],o):"object"==typeof exports?exports["a-propos/notre-equipe"]={}:e["a-propos/notre-equipe"]={}}(this,()=>({}));
